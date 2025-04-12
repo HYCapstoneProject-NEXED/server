464cc02ce9001693dddc09ed74bf0c9965419006
lgr0831 <qfrub0@gmail.com>
2025-04-13 01:24:03 +0900

    feat: [Customer/EditClass] 결함 클래스 수정 API 구현

--- domain/defect_class/defect_class_crud.py
+++ domain/defect_class/defect_class_crud.py
@@ -1,18 +1,34 @@
 from sqlalchemy.orm import Session
-from database.models import DefectClasses  # 모델은 여기에 정의돼 있다고 가정
-from .defect_class_schema import DefectClassCreate
+from database.models import DefectClasses
+from fastapi import HTTPException
+from domain.defect_class import defect_class_schema
 
 
 def get_all_defect_classes(db: Session):
     return db.query(DefectClasses).order_by(DefectClasses.created_at).all()
 
 
-def create_defect_class(db: Session, defect_class: DefectClassCreate) -> DefectClasses:
+def create_defect_class(db: Session, defect_class: defect_class_schema.DefectClassCreate) -> DefectClasses:
     db_class = DefectClasses(
         class_name=defect_class.class_name,
         class_color=defect_class.class_color
     )
     db.add(db_class)
+    db.commit()
+    db.refresh(db_class)
+    return db_class
+
+
+def update_defect_class(db: Session, class_id: int, update_data: defect_class_schema.DefectClassUpdate):
+    db_class = db.query(DefectClasses).filter(DefectClasses.class_id == class_id).first()
+    if not db_class:
+        raise HTTPException(status_code=404, detail="Defect class not found")
+
+    if update_data.class_name is not None:
+        db_class.class_name = update_data.class_name
+    if update_data.class_color is not None:
+        db_class.class_color = update_data.class_color
+
     db.commit()
     db.refresh(db_class)
     return db_class
--- domain/defect_class/defect_class_schema.py
+++ domain/defect_class/defect_class_schema.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, Field
 from datetime import datetime
+from typing import Optional
+
 
 # DefectClass 조회 응답 모델
 class DefectClassResponse(BaseModel):
@@ -12,7 +14,14 @@ class DefectClassResponse(BaseModel):
     class Config:
         orm_mode = True
 
+
 # 결함 클래스 추가 응답 모델
 class DefectClassCreate(BaseModel):
     class_name: str = Field(..., example="Scratch")
     class_color: str = Field(..., example="#dbe4ff")
+
+
+# 수정 요청용 Pydantic 모델
+class DefectClassUpdate(BaseModel):
+    class_name: Optional[str] = Field(None, example="New Name")
+    class_color: Optional[str] = Field(None, example="#abcdef")
