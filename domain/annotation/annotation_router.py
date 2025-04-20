ae28125eda180f2b5b88063165e8c4823b8e3bec
lgr0831 <qfrub0@gmail.com>
2025-04-21 05:28:48 +0900

    feat: [Customer/DefectData] 결함 데이터 목록 조회 API 구현

--- domain/annotation/annotation_router.py
+++ domain/annotation/annotation_router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database.database import get_db
 from domain.annotation import annotation_crud, annotation_schema
+from typing import List
 
 
 router = APIRouter(
@@ -12,3 +13,10 @@ router = APIRouter(
 @router.get("/summary", response_model=annotation_schema.DefectSummaryResponse)
 def get_defect_summary_with_change(db: Session = Depends(get_db)):
     return annotation_crud.get_defect_summary(db)
+
+@router.post("/defect-data/list", response_model=List[annotation_schema.DefectDataItem])
+def get_defect_data_list_api(
+    filters: annotation_schema.DefectDataFilter,
+    db: Session = Depends(get_db)
+):
+    return annotation_crud.get_filtered_defect_data_list(db, filters)
